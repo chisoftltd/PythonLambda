621b19d7d907af0db6ae5e3d5ab972e6e3462a2f
chisoftltd <chisoftltd@gmail.com>
2021-02-22 18:55:24 +0100

    first commit

---
+++ PythonLambda.py
@@ -0,0 +1,25 @@
+#Python Lambda
+x = lambda a : a + 10
+print(x(55))
+print()
+
+x = lambda a, b : a * b + b / a
+print(x(2, 4))
+
+# Why Use Lambda Functions?
+# anonymous function
+def myfunc(n):
+    return lambda a : a / n
+
+mydivider = myfunc(1.2)
+print(mydivider(3))
+print()
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(0.2)
+mytripler = myfunc(3.2)
+
+print(mydoubler(1.1))
+print(mytripler(11.0))
